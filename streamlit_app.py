4d75253e2a9482cb49229822eb0978c5dddc78c6
tai29092002 <84424819+tai29092002@users.noreply.github.com>
2025-06-01 23:59:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ from st_flexible_callout_elements import flexible_callout
 
 custom_css = {
     ".ag-root-wrapper": {"border-radius": "8px"},
-    ".ag-header": {"font-size": "16px", "background-color": "#FFDAB3"},
+    ".ag-header": {"font-size": "16px", "background-color": "#FFF2EB"},
     ".ag-cell": {"font-size": "14px", "padding": "4px"},
     ".ag-row-hover": {"background-color": "#FCE7C8"},
 }  
